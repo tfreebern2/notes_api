5fee58eeda5f7842abe81be9f9bf6e5d7473660b
Timothy Freebern <tfreebern2@gmail.com>
2017-05-20 22:46:41 +0000

    4 Passing Tests Note Creation

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,4 +1,15 @@
 class NotesController < ApplicationController
   def index
   end
+
+  def create
+    note = Note.create(note_params)
+    render json: note, status: :created
+  end
+
+  private
+
+  def note_params
+    params.require(:note).permit(:title, :content)
+  end
 end
