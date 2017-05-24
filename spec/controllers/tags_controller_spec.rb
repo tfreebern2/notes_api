4d2d54dda7ab903a936458929a23f97b8350828c
Timothy Freebern <tfreebern2@gmail.com>
2017-05-24 21:07:57 +0000

    Added Tags and Validation Testing

--- spec/controllers/tags_controller_spec.rb
+++ spec/controllers/tags_controller_spec.rb
@@ -15,4 +15,20 @@ RSpec.describe TagsController, type: :controller do
       expect(@note.tags.first.name).to eq('Crazy')
     end
   end
+
+  describe "tags#create action validaions" do
+    before do
+      note = FactoryGirl.create(:note)
+      post :create, params: { tag: {name: ''}, note_id: note.id }
+    end
+
+    it "should properly deal with validation error" do
+      expect(response).to have_http_status(:unprocessable_entity)
+    end
+
+    it "should return error json on validation error" do
+      json = JSON.parse(response.body)
+      expect(json['errors']['name'][0]).to eq("can't be blank")
+    end
+  end
 end
